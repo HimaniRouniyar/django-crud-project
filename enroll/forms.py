d7afe43bd0c6543e224b5593aead5abab3709d07
himani <himanirouniyar15@gmail.com>
2026-05-22 05:15:48 +0545

    Added update and delete functionality

--- enroll/forms.py
+++ enroll/forms.py
@@ -8,5 +8,5 @@ class StudentRegistartion(forms.ModelForm):
         widgets = {
             'name': forms.TextInput(attrs={'class':'form-control'}),
             'email': forms.EmailInput(attrs={'class':'form-control'}),
-            'password': forms.PasswordInput(attrs={'class':'form-control'}),
+            'password': forms.PasswordInput(render_value=True,attrs={'class':'form-control'}),
         }
